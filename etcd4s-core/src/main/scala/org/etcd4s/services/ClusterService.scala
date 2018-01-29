c0380ffe5662c480d2b10bbcd09ba1cae33afe4d
mingchun.or <mingchuno@gmail.com>
2018-01-29 19:18:04 +0800

    add extra API for ClusterService

--- etcd4s-core/src/main/scala/org/etcd4s/services/ClusterService.scala
+++ etcd4s-core/src/main/scala/org/etcd4s/services/ClusterService.scala
@@ -3,22 +3,38 @@ package org.etcd4s.services
 import org.etcd4s.pb.etcdserverpb.ClusterGrpc.{Cluster, ClusterStub}
 import org.etcd4s.pb.etcdserverpb._
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
 private[etcd4s] class ClusterService(protected val stub: ClusterStub) extends Cluster {
   override def memberAdd(request: MemberAddRequest): Future[MemberAddResponse] = {
     stub.memberAdd(request)
   }
 
+  def memberAdd(peerURLs: Seq[String]): Future[MemberAddResponse] = {
+    memberAdd(MemberAddRequest(peerURLs))
+  }
+
   override def memberRemove(request: MemberRemoveRequest): Future[MemberRemoveResponse] = {
     stub.memberRemove(request)
+  }
+
+  def memberRemove(iD: Long)(implicit ec: ExecutionContext): Future[Seq[Member]] = {
+    memberRemove(MemberRemoveRequest(iD)).map(_.members)
   }
 
   override def memberUpdate(request: MemberUpdateRequest): Future[MemberUpdateResponse] = {
     stub.memberUpdate(request)
   }
 
+  def memberUpdate(iD: Long, peerURLs: Seq[String])(implicit ec: ExecutionContext): Future[Seq[Member]] = {
+    memberUpdate(MemberUpdateRequest(iD, peerURLs)).map(_.members)
+  }
+
   override def memberList(request: MemberListRequest): Future[MemberListResponse] = {
     stub.memberList(request)
+  }
+
+  def memberList()(implicit ec: ExecutionContext): Future[Seq[Member]] = {
+    memberList(MemberListRequest()).map(_.members)
   }
 }
