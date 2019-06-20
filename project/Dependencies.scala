b1134c03a2b8bd47f63ae9228133a39df39ddd57
Mike Chen <517926804@qq.com>
2019-06-20 16:34:44 +0800

    Update dependencies to support scala 2.13.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,13 +2,13 @@ import sbt._
 import scalapb.compiler.Version._
 
 object Dependencies {
-  private val scala_test    = "org.scalatest" %% "scalatest" % "3.0.5"
+  private val scala_test    = "org.scalatest" %% "scalatest" % "3.0.8"
   // gRPC
   private val grpc_netty    = "io.grpc" % "grpc-netty" % grpcJavaVersion
   private val grpc_runtime  = "com.thesamet.scalapb" %% "scalapb-runtime-grpc" % scalapbVersion
 
   // akka stream
-  private val akka_version = "2.5.18"
+  private val akka_version = "2.5.23"
   private val akka_stream           = "com.typesafe.akka" %% "akka-stream" % akka_version
   private val akka_stream_testkit   = "com.typesafe.akka" %% "akka-stream-testkit" % akka_version
   private val akka_test_kit         = "com.typesafe.akka" %% "akka-testkit" % akka_version
