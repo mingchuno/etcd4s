1700feb22b49aa8b23d3efb98f31ca83b2b2136c
mingchun.or <mingchuno@gmail.com>
2020-01-16 16:03:46 +0800

    add response signature for watch API

--- etcd4s-core/src/main/scala/org/etcd4s/rpc/WatchRpc.scala
+++ etcd4s-core/src/main/scala/org/etcd4s/rpc/WatchRpc.scala
@@ -5,5 +5,7 @@ import org.etcd4s.pb.etcdserverpb._
 import io.grpc.stub.StreamObserver
 
 private[etcd4s] class WatchRpc(protected val stub: WatchStub) extends Watch {
-  override def watch(responseObserver: StreamObserver[WatchResponse]) = stub.watch(responseObserver)
+  override def watch(
+      responseObserver: StreamObserver[WatchResponse]
+  ): StreamObserver[WatchRequest] = stub.watch(responseObserver)
 }
